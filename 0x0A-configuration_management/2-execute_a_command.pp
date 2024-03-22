41c46acf101b8ec6790c9c68989c8ce4d3672e86
ehab-al-Mawardi <ihabalmawardi@gmail.com>
2024-03-22 08:39:39 -0700

    KILL ME NOW

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,7 @@
-# Executes a bash command
-exec { 'kill':
-  command => 'pkill -f killmenow',
+# Define an exec resource to kill the process named "killmenow"
+exec { 'killmenow':
+  command     => 'pkill -f killmenow',
+  refreshonly => true, # Only run the command when notified
+  onlyif      => 'pgrep killmenow', # Check if the process is running before attempting to kill
   path    => ['/usr/bin', '/usr/sbin']
 }
